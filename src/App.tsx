7ada59eadd6cf0caea586c65954d5798450a785e
douglas rocha <douglashidekazu15@gmail.com>
2025-12-19 19:50:45 +0900

    create: Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import Text from "./components/text"
-import trashIcon from "./assets/icons/Trash-Regular.svg"
+import Icon from "./components/icons"
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react"
+import CheckIcon from "./assets/icons/Check-Regular.svg?react"
+import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react"
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react"
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
+import XIcon from "./assets/icons/X-Regular.svg?react"
 
 export default function App() {
   return (
@@ -11,7 +17,12 @@ export default function App() {
       </div>
 
       <div className="flex gap-1">
-        <img src={trashIcon} />
+        <Icon svg={TrashIcon} className= "fill-pink-base"/>
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate/>
+        <Icon svg={XIcon} />
       </div>
     </div>
   )
