6fe80f0029e7e162208a1a9f49b60a1b152b8f03
douglas rocha <douglashidekazu15@gmail.com>
2025-12-19 18:53:20 +0900

    add: vite-plugin-svgr and cva library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Text from "./components/text"
+import trashIcon from "./assets/icons/Trash-Regular.svg"
 
 export default function App() {
   return (
-    <div>
-      <Text as="h1" variant="body-sm-bold" className="text-pink-base"> Ola Mundo !</Text>
-      <Text as="h1" className="text-green-base"> Ola Mundo !</Text>
-      <Text as="h1" variant="body-md-bold"> Ola Mundo !</Text>
+    <div className="grid gap-3">
+      <div>
+        <Text as="h1" variant="body-sm-bold" className="text-pink-base"> Ola Mundo !</Text>
+        <Text as="h1" className="text-green-base"> Ola Mundo !</Text>
+        <Text as="h1" variant="body-md-bold"> Ola Mundo !</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <img src={trashIcon} />
+      </div>
     </div>
   )
 }
