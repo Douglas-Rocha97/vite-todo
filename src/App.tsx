b1e71a9fe6f8ad9f54898b95b3a4031b2f82f067
douglas rocha <douglashidekazu15@gmail.com>
2025-12-19 18:41:30 +0900

    create: text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Text from "./components/text"
+
 export default function App() {
   return (
-    <div className="text-gray-dark">
-      Ola mundo!
+    <div>
+      <Text as="h1" variant="body-sm-bold" className="text-pink-base"> Ola Mundo !</Text>
+      <Text as="h1" className="text-green-base"> Ola Mundo !</Text>
+      <Text as="h1" variant="body-md-bold"> Ola Mundo !</Text>
     </div>
   )
 }
